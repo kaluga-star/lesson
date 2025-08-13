c2be3d893ce8c64b5a0367926362df35730d077d
kaluga-star <odtukaluga@gmail.com>
2025-08-13 16:32:27 +0300

    Add files via upload

---
+++ lesson.js
@@ -0,0 +1,45 @@
+export const scheduleData = [
+  //, stop: 'Занятия не будет' 
+  {  day: 'Понедельник', date: new Date(2025, 8, 1), lessons:[
+      { name: 'Группа 3', start: '9:00', end: '10:00', stop: ''  },
+      { name: 'Группа 8', start: '10:20', end: '11:50' , stop: ''  },
+      { name: 'Группа 5', start: '13:30', end: '14:30', stop: ''  },
+      { name: 'Группа 1', start: '14:50', end: '15:50', stop: ''   },
+      { name: 'Группа 7', start: '16:10', end: '17:40', stop: ''   },
+    ]},
+  {  day: 'Вторник',  date: new Date(2025, 8, 2), lessons: [
+      { name: 'Группа 1', start: '9:00', end: '10:30' },
+      { name: 'Группа 1', start: '10:45', end: '12:15' },
+      { name: 'Группа 1', start: '13:00', end: '14:30' },
+      { name: 'Группа 1', start: '14:45', end: '16:15' },
+      { name: 'Группа 1', start: '16:30', end: '18:00' },
+    ]},
+  {   day: 'Среда', date: new Date(2025, 8, 3), lessons:[
+      { name: 'Группа 1', start: '9:00', end: '10:30' },
+      { name: 'Группа 1', start: '10:45', end: '12:15' },
+      { name: 'Группа 1', start: '13:00', end: '14:30' },
+      { name: 'Группа 1', start: '14:45', end: '16:15' },
+      { name: 'Группа 1', start: '16:30', end: '18:00' },
+    ]},
+  {   day: 'Четверг', date: new Date(2025, 8, 4), lessons:[
+      { name: 'Группа 1', start: '9:00', end: '10:30' },
+      { name: 'Группа 1', start: '10:45', end: '12:15' },
+      { name: 'Группа 1', start: '13:00', end: '14:30' },
+      { name: 'Группа 1', start: '14:45', end: '16:15' },
+      { name: 'Группа 1', start: '16:30', end: '18:00' },
+    ]},
+  {   day: 'Пятница', date: new Date(2025, 8, 5), lessons:[
+      { name: 'Группа 1', start: '9:00', end: '10:30' },
+      { name: 'Группа 1', start: '10:45', end: '12:15' },
+      { name: 'Группа 1', start: '13:00', end: '14:30' },
+      { name: 'Группа 1', start: '14:45', end: '16:15' },
+      { name: 'Группа 1', start: '16:30', end: '18:00' },
+    ]},
+  {   day: 'Суббота', date: new Date(2025, 8, 6), lessons: [
+      { name: 'Группа 1', start: '9:00', end: '10:30' },
+      { name: 'Группа 1', start: '10:45', end: '12:15' },
+      { name: 'Группа 1', start: '13:00', end: '14:30' },
+      
+    ]},
+];
+
